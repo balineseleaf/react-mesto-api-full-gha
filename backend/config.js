f8a753b0c5a7b7de1d71b1ca3332d5cbeaf1c0c3
Mikhail Zinchik <MVMGroup888@yandex.ru>
2023-09-22 20:52:36 +0300

    fx regex

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-useless-escape
-const { REG_URL = /https?:\/\/(www\.)?[\w\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+\#?$/i } = process.env;
+const { REG_URL = /^(https?:\/\/(www\.)?[\w\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\=]+\#?)$/i } = process.env;
 const { DB_URL = 'mongodb://localhost:27017/mestodb' } = process.env;
 module.exports = {
   REG_URL,
